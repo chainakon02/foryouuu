6890144ada0ef683ecf0d4ecb3323951fecde52b
chainakon02 <chainakonsarisee@gmail.com>
2024-11-18 03:34:08 +0700

    Update password.js

--- js/password.js
+++ js/password.js
@@ -1,16 +1,16 @@
 document.getElementById("submit-password").addEventListener("click", function () {
     const passwordInput = document.getElementById("password-input").value;
-    const correctPassword = "1234"; // รหัสผ่านที่ถูกต้อง
+    const correctPassword = "05012002"; 
   
     if (passwordInput === correctPassword) {
-      // ซ่อนฟอร์มรหัสผ่าน
+     
       document.querySelector(".password-container").style.display = "none";
-      // แสดงเนื้อหาหลัก
+     
       document.querySelector(".container").style.display = "block";
-      // แสดงเนื้อหาของ additional-content
+      
       document.getElementById("additional-content").style.display = "block";
     } else {
-      // ถ้ารหัสผ่านผิด แสดงข้อความผิด
+     
       const errorMessage = document.getElementById("error-message");
       errorMessage.style.display = "block";
       setTimeout(() => {
